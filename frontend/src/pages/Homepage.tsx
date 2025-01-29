76c907158efcc2d9c19b4cf463a5424dc6338e0e
Etienne Tournier Rigaudy <e.tournier.rigaudy@gmail.com>
2025-01-29 11:24:17 +0100

    fix style

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -3,7 +3,7 @@ import ListSumCryptos from "../components/ListSum";
 
 const Homepage: React.FC = () => {
   return (
-    <div className="bg-white w-3/4 h-screen text-zinc-800 p-5">
+    <div className="bg-white w-3/4 h-full text-zinc-800 p-5">
       <div className="text-zync-800 rounded-md text-5xl text-center m-1 font-bold flex-row">
         CryptoFolio
       </div>
